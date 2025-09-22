8cc6335d3887ccfd17f462abefaf1d65f756afe1
Bhoomi Naik <bhoominaik16@gmail.com>
2025-09-22 20:55:40 +0530

    update

--- Portfolio/src/Pages/Home.jsx
+++ Portfolio/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function Home(){
 
     return(
-        <section className='max-h-[90vh] md:p-[5vh] p-[2vh]'>
+        <section className='xl:max-h-[90vh] md:p-[5vh] p-[2vh]'>
             <div >
                 <div className="min-h-[43vh] md:min-h-[55vh] mx-auto bg-white flex items-center md:items-center justify-center ">
                     <div className='max-w-[50%]'>
@@ -22,7 +22,7 @@ function Home(){
                         <img className='max-h-[55vh]' src={Bhoomi_Anime} alt='Bhoomi Picture' />
                     </div>
                 </div>
-                <div className="min-h-[37vh] border-2 border-violet-200 shadow-2xl  bg-violet-200 grid grid-cols-1 md:grid-cols-3 md:gap-20 items-center text-center mx-[5%] p-[2%] md:min-h-[25vh]">
+                <div className="min-h-[50vh] border-2 border-violet-200 shadow-2xl  bg-violet-200 grid grid-cols-1 md:grid-cols-3 md:gap-20 items-center text-center mx-[5%] p-[2%] md:min-h-[25vh]">
                 <div>
                     <h1 className='text-2xl md:text-3xl font-bold m-4'>Full Stack Web Developer</h1>
                     <p className='text-md'>Developing creative and engaging UI designs using <span className='font-semibold'>React</span> and gaining proficiency in <span className='font-semibold'>MERN Stack</span></p>
